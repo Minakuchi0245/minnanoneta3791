cff523179942c39e85adf074d2094f48594abb6b
Minakuchi0245 <kamilia3791@gmail.com>
2022-05-31 20:52:40 +0900

    ユーザーモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :netas
+  has_many :comments
+  has_many :likes
+
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
+
+  encrypted_password = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: encrypted_password, message: 'は無効です 英字と数字の両方を含めてください'
 end
